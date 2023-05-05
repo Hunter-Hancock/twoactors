6f167b2e0bafc73d66e79370860eff06fb06ddf4
MildlyGithub <hunterhancock1141@gmail.com>
2023-05-04 19:40:54 -0500

    Please Fix

--- src/app/Components/SearchComponent.tsx
+++ src/app/Components/SearchComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import Credit from "../Interfaces/Credit";
 import Result from "../Interfaces/Result";
 import MovieComponent from "./MovieComponent";
